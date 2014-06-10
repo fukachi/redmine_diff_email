76d85ccf703f9f35ea117225d66df819eb0173d9
Lukas Pirl <git@lukas-pirl.de>
2014-06-10 21:50:32 +0200

    increased version number

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_diff_email do
   name 'Redmine Diff Email Plugin'
   author 'Lukas Pirl (orig. by Kah Seng Tay, Sergey Generalov, Lamar, Ivan Evtuhovich, cou2jpn)'
   description 'This is a plugin for Redmine that sends diff emails on commits.'
-  version '0.2.1'
+  version '0.3.0'
   url 'https://github.com/lpirl/redmine_commit_email'
   requires_redmine :version_or_higher => '2.5.1'
 end
