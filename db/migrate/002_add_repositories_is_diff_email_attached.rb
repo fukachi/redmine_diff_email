111b9f157b1446f44ee03967720bd04b1db3367c
root <root@dev.jbox-web.fr>
2014-07-14 15:10:44 +0200

    Fix ERROR:  operator does not exist: boolean = integer with Postgres

--- db/migrate/002_add_repositories_is_diff_email_attached.rb
+++ db/migrate/002_add_repositories_is_diff_email_attached.rb
@@ -1,7 +1,7 @@
 class AddRepositoriesIsDiffEmailAttached < ActiveRecord::Migration
   def self.up
     add_column :repositories, :is_diff_email_attached, :boolean, :default => false, :null => false
-    Repository.update_all("is_diff_email_attached = 1", "is_diff_email = 1")
+    Repository.update_all("is_diff_email_attached = true", "is_diff_email = true")
   end
 
   def self.down
